ef5289c1415f3f9164bca9e98cb9ce40e5f5e55b
Ayush Soni <mr.soni1002001@gmail.com>
2022-01-27 01:13:20 +0530

    added ceiling & floor number

---
+++ Java/src/BinarySearchInterviewQuestions_11/CeilingOfANumber.java
@@ -0,0 +1,36 @@
+package BinarySearchInterviewQuestions_11;
+
+public class CeilingOfANumber {
+    public static void main(String[] args){
+        int[] num = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = CeilingNumber(num, target);
+        System.out.println(ans);
+    }
+//        cerling of a number: smallest number >= target
+//    return the index of smallest number which is greater than or equal to target
+    static int CeilingNumber(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+//        but what if the target is greater than the greatest number in the array
+//        if(target > arr.length){
+//            return -1;
+//        }
+
+        while(start <= end){
+//            find middle element
+            int middle = start + (end - start) / 2;
+            if(target < arr[middle]){
+                end = middle - 1;
+            }
+            else if(target > arr[middle]){
+                start = middle + 1;
+            }
+            else{
+                return middle;
+            }
+        }
+        return start;
+    }
+}
