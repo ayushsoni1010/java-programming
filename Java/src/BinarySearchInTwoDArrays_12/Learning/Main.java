20437f18735484374ed6adad15665c7a4913aab7
Ayush Soni <mr.soni1002001@gmail.com>
2022-02-04 21:48:24 +0530

    added 2d array problems

---
+++ Java/src/BinarySearchInTwoDArrays_12/Learning/Main.java
@@ -0,0 +1,9 @@
+package BinarySearchInTwoDArrays_12.Learning;
+
+public class Main {
+    public static void main(String[] args) {
+        String word = "Geeks";
+        int result = word.codePointAt(0);
+        System.out.println(result);
+    }
+}
